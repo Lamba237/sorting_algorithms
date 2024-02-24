965ec10e9671b8cdd800d163748a9d335a1ca52c
Lamba237 <simontanyi2004@gmail.com>
2024-02-24 13:19:41 -0800

    updates

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,7 +12,7 @@ void shell_sort(int *array, size_t size)
 	size_t increment;
 	int tmp;
 
-	for (increment = 1; increment <= size / 3; increment = increment * 3 + 1)
+	for (increment = size / 2; increment > 0; increment = increment / 2)
 	{
 		for (i = increment; i < size; i++)
 		{
